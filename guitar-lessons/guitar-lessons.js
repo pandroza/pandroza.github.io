9b9322ee5544917a9a43d491109b6698acf1e5f1
pandroza <60747184+pandroza@users.noreply.github.com>
2021-02-22 05:31:59 +0000

    Add files via upload

---
+++ guitar-lessons/guitar-lessons.js
@@ -0,0 +1,24 @@
+var h = ["Anyone can play guitar!","No matter what your age, you can learn with me!","Lessons available online or in person!", "Want to learn but not confident? Don't worry! I can help!"]
+var hc = 0;
+document.getElementById("about");
+var startTime = new Date().getTime();
+var titles = function() {
+    var currTime = new Date().getTime();
+    var count = ((currTime - startTime)/1000);
+  if (count > 0.5 && count < 2.0) {
+    about.innerHTML = h[hc];
+    about.style.opacity = count-0.5;
+  }
+  if (count > 8 && count < 10) {
+    about.style.opacity = 1-(count-8);
+  }
+  if (count > 10) {
+    hc ++;
+    if(hc === 3){
+      hc = 0;
+    }
+    startTime = new Date().getTime();
+  }
+  window.requestAnimationFrame(titles);
+};
+titles();
