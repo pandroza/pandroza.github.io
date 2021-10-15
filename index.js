c1f64e7a97269b89f1ed1cce6f1d7bca7ebc7ed6
pandroza <60747184+pandroza@users.noreply.github.com>
2021-10-15 03:08:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ var images = ['./FA-icons/johnpeel5.png',
                 './FA-icons/canyon.png',
                 './FA-icons/survey.png',
                 './FA-icons/technic.png',
-                './FA-icons/floaty-octocat.png',
+                'floaty-octocat.png',
                 './FA-icons/thispage.png'
             ];
 var myHex;
